ea406963cf73463ea7e7f676467eafb626a76820
chamod0 <chamod.dilshan97@gmail.com>
2020-04-15 21:57:22 +0530

    main code small update

--- HelthCare/src/com/HelthCare.java
+++ HelthCare/src/com/HelthCare.java
@@ -1,5 +1,26 @@
 package com;
 
-public class HelthCare {
+import model.HelthCareModel;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
 
+
+@Path("/HelthCare")
+public class HelthCare
+{
+	HelthCareModel HelthCareObj = new HelthCareModel();
+		@GET
+		@Path("/")
+		@Produces(MediaType.TEXT_HTML)
+	public String readItems()
+		{
+			return "Hello";
+		}
 }
